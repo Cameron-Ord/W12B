be284340e6c3786ab4d43d7a0825a8d1a61118d9
cameron <hammerbro_97@hotmail.com>
2023-04-08 15:04:48 -0600

    working on post click function

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+let injector = document.querySelector(`#section_main`);
+
+injector.insertAdjacentHTML(`beforeend`,
+`<article>
+<span>
+<input type="value" id="post_input">
+<button class="POST">POST</button>
+
+</span>
+<span>
+<input type="value" id="patch_input">
+<button class="PATCH">PATCH</button>
+
+</span>
+<span>
+<input type="value" id="delete_input">
+<button class="DELETE">DELETE</button>
+
+</span>
+</article>`
+);
+
+
+
+
+function POST_click(details){
+
+    let post_input = document.querySelector(`#post_input`);
+    let post_input_value = post_input[`value`];
+}; 
+
