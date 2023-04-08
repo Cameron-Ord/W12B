2df2274e3d20a30dadc1628ad30fe2ebe2737ca8
cameron <hammerbro_97@hotmail.com>
2023-04-08 15:08:23 -0600

    working on post click function

--- js/app.js
+++ js/app.js
@@ -28,5 +28,19 @@ function POST_click(details){
 
     let post_input = document.querySelector(`#post_input`);
     let post_input_value = post_input[`value`];
+
+    axios.request({
+
+        url: `https://jsonplaceholder.typicode.com/posts`,
+
+        method: `POST`,
+
+        data: {
+
+            title: post_input_value
+            
+        }
+
+    }).then(POSTsucess).catch(POSTfailure);
 }; 
 
